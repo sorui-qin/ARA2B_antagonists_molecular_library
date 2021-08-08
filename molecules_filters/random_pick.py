e50b6c5b9f2a746aba8e5e7c93c3509c0980469a
sorui-qin <842220994@qq.com>
2021-08-08 23:24:24 +0800

    20210808update

--- molecules_filters/random_pick.py
+++ molecules_filters/random_pick.py
@@ -14,9 +14,9 @@ def write(filename, n):
             pass
     
     def to_smi(filename, li, n):
-        with open('{0}+random+{1}.smi'.format(filename_check(filename), n),'a+') as f:
+        with open('{0}_random_{1}.smi'.format(filename_check(filename), n),'a+') as f:
             for i in li:
-                f.write(i[0] + '\n')
+                f.write(i + '\n')
     to_smi(filename, read(filename,n), n)
 
 if __name__ == '__main__':
