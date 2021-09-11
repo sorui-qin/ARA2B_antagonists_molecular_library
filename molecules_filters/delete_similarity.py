ec9a1baa84376e9509c8f5ff8d4db4fbaeff3bea
sorui-qin <842220994@qq.com>
2021-09-11 10:14:57 +0800

    210911update

---
+++ molecules_filters/delete_similarity.py
@@ -0,0 +1,31 @@
+'''
+This program is in order to delete the same molecules in a molecular library in txt/smi format.
+Molecules should be presented in SMILES.
+RDKit is needed to run this program.
+'''
+
+import rdkit
+import rdkit.Chem as rkc
+
+def standrad_smi(smile):
+    return rkc.MolToSmiles(rkc.MolFromSmiles(smile))
+
+def delete_same(li):
+    fin_li=[]
+    for smi in li:
+        fin_li.append(standrad_smi(smi))
+        del_li = sorted(set(fin_li),key=fin_li.index)
+    return del_li
+
+def write(filename):
+    with open(filename,'r') as f:
+        w=f.read()
+        li=w.split('\n')
+    fin_li=delete_same(li)
+    with open(filename,'w') as fi:
+        for i in fin_li:
+            fi.write(i+'\n')
+
+
+filename=(r'D:\Research\A2B\Pharmacophore\allmole74.smi')
+write(filename)
